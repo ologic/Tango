c93027d3ad7e01f49a3bca7eea03ea7c03eedc46
Brandon Blodget <brandon@ologicinc.com>
2014-06-17 16:01:59 -0700

    First version of ROSTango workspace.

---
+++ src/rostango/android_tutorial_pubsub/src/main/gen/org/ros/android/android_tutorial_pubsub/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.ros.android.android_tutorial_pubsub;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/rostango/tango_vio/src/main/java/com/ologicinc/rostango/tango_vio/TangoVio.java
@@ -0,0 +1,15 @@
+package com.ologicinc.rostango.tango_vio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TangoVio extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
