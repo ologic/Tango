f7078ad70c6a71825cd1c756c02bb4ceead3b280
Brandon Blodget <brandon@ologicinc.com>
2014-06-18 15:00:08 -0700

    Finished adding the VIO data.

--- ROSTango/src/rostango/tango_vio/src/main/java/com/ologicinc/rostango/tango_vio/TangoVio.java
+++ ROSTango/src/rostango/tango_vio/src/main/java/com/ologicinc/rostango/tango_vio/TangoVio.java
@@ -1,29 +1,47 @@
 package com.ologicinc.rostango.tango_vio;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.motorola.atap.androidvioservice.VinsServiceHelper;
 
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
+import org.ros.message.Time;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 import org.ros.tf2_ros.StaticTransformBroadcaster;
 import org.ros.tf2_ros.TransformBroadcaster;
 
+import java.io.File;
+
+import geometry_msgs.Quaternion;
 import geometry_msgs.TransformStamped;
+import geometry_msgs.Vector3;
 
 public class TangoVio extends RosActivity
 {
     // Set up Transform Broadcasters
     private final TransformBroadcaster mTB = new TransformBroadcaster();
     private final StaticTransformBroadcaster mSTB = new StaticTransformBroadcaster();
+    private Quaternion mQuat;
+    private Vector3 mPos;
 
     // Set up messages
     private TransformStamped tfs = mTB.newMessage();
 
     // Set up VIO format
+    private VinsServiceHelper mVinsServiceHelper;
     int coordinateConvention = VinsServiceHelper.STATEFORMAT_UNITY;
+
+    private int superFrameBufferSize;
+    private byte[] b;
+    private Bitmap bm;
 
     public TangoVio() {
         super("TangoVio", "TangoVio");
@@ -34,7 +52,96 @@ public class TangoVio extends RosActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        /* Read VIO data from VINs service helper */
+        mVinsServiceHelper = new VinsServiceHelper(this);
+
+        /* Check for the calibration file. */
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/calibration.xml");
+        if (!file.exists()) {
+            Toast.makeText(getApplicationContext(), "Could not load " +
+                    "/sdcard/calibration.xml, using default calibration." +
+                    " For best performance install the file and restart the " +
+                    "app.", Toast.LENGTH_LONG).show();
+        }
+
+        /* Set the version code and name on the title bar. */
+        try {
+            PackageInfo pkgInfo =
+                    getPackageManager().getPackageInfo(getPackageName(), 0);
+            setTitle("Vins Service Tester v" + pkgInfo.versionName);
+        } catch (NameNotFoundException e) {
+            // App is looking for its own package name - don't need to handle this.
+        }
+
         setContentView(R.layout.main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (mVinsServiceHelper != null) {
+                        Thread.sleep(30);
+
+                        if (superFrameBufferSize == 0) {
+                            try {
+                                superFrameBufferSize = mVinsServiceHelper.getSuperFrameWidth()
+                                        * mVinsServiceHelper.getSuperFrameHeight();
+                                b = new byte[superFrameBufferSize];
+                                bm = Bitmap.createBitmap(mVinsServiceHelper.getSuperFrameWidth(),
+                                        mVinsServiceHelper.getSuperFrameHeight(), Bitmap.Config.ALPHA_8);
+                                Log.i("AndroidVinsServiceTester", "Superframe is " + superFrameBufferSize);
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        final double[] state = getState(coordinateConvention);
+                        if (state.length == 0) { continue; }
+
+                        // Generate the TF message
+                        updateTf(state);
+
+                        // XXX old stuff
+
+                        //
+                        //final DecimalFormat fourDec = new DecimalFormat("0.0000");
+                        //runOnUiThread(new Runnable() {
+                        //@Override
+                        //public void run() {
+                        //if (state != null) {
+                        //try {
+                        //deviceConvention.setText(formatToString(coordinateConvention));
+                        //rot0.setText(fourDec.format(state[0]));
+                        //rot1.setText(fourDec.format(state[1]));
+                        //rot2.setText(fourDec.format(state[2]));
+                        //rot3.setText(fourDec.format(state[3]));
+                        //pos0.setText(fourDec.format(state[4]));
+                        //pos1.setText(fourDec.format(state[5]));
+                        //pos2.setText(fourDec.format(state[6]));
+                        //stats0.setText(fourDec.format(state[7]));
+                        //stats1.setText(fourDec.format(state[8]));
+                        //stats2.setText(fourDec.format(state[9]));
+                        //stats3.setText(fourDec.format(state[10]));
+                        //stats4.setText(fourDec.format(state[11]));
+                        //stats5.setText(fourDec.format(state[12]));
+                        //} catch (Exception e) {
+                        //e.printStackTrace();
+                        //}
+                        //}
+                        //}
+                        //});
+                        //
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
     }
 
     @Override
@@ -46,5 +153,47 @@ public class TangoVio extends RosActivity
         nodeMainExecutor.execute(mTB, nodeConfiguration);
         nodeMainExecutor.execute(mSTB, nodeConfiguration);
 
-       }
+    }
+
+    public void updateTf(double[] state) {
+
+        mQuat.setW(state[0]);
+        mQuat.setX(state[1]);
+        mQuat.setY(state[2]);
+        mQuat.setZ(state[3]);
+
+        mPos.setX(state[4]);
+        mPos.setY(state[5]);
+        mPos.setZ(state[6]);
+
+        tfs.getTransform().setRotation(mQuat);
+        tfs.getTransform().setTranslation(mPos);
+
+        tfs.getHeader().setFrameId("/world");
+        tfs.setChildFrameId("/base_link");
+        long lt = System.currentTimeMillis();
+        Time t = new Time((int)(lt / 1e3), (int)((lt % 1e3) *1e6));
+        tfs.getHeader().setStamp(t);
+        mTB.sendTransform(tfs);
+
+    }
+
+    double [] getState(int convention) {
+        switch (convention) {
+            case VinsServiceHelper.STATEFORMAT_UNITY:
+                return mVinsServiceHelper.getStateInUnityFormat();
+            case VinsServiceHelper.STATEFORMAT_ESTIMATOR:
+                return mVinsServiceHelper.getStateInEstimatorFormat();
+            case VinsServiceHelper.STATEFORMAT_OPENGL:
+                return mVinsServiceHelper.getStateInOpenGLFormat();
+            case VinsServiceHelper.STATEFORMAT_FULL_STATE:
+                return mVinsServiceHelper.getStateInFullStateFormat();
+            case VinsServiceHelper.STATEFORMAT_ANDROID:
+                return mVinsServiceHelper.getStateInAndroidFormat();
+            default:
+                return new double[] {Double.NaN, Double.NaN, Double.NaN, Double.NaN,
+                        Double.NaN, Double.NaN, Double.NaN};
+        }
+    }
+
 }
