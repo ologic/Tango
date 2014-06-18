32a42066358be4af59c0d605d08d4d94b73e25eb
Rohan Agrawal <send2arohan@gmail.com>
2014-06-18 13:43:23 -0700

    Commented the code

--- ROSTango/src/rostango/tango_vio/src/main/java/com/ologicinc/rostango/tango_vio/TangoVio.java
+++ ROSTango/src/rostango/tango_vio/src/main/java/com/ologicinc/rostango/tango_vio/TangoVio.java
@@ -2,6 +2,8 @@ package com.ologicinc.rostango.tango_vio;
 
 import android.os.Bundle;
 
+import com.motorola.atap.androidvioservice.VinsServiceHelper;
+
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
 import org.ros.node.NodeConfiguration;
@@ -9,16 +11,18 @@ import org.ros.node.NodeMainExecutor;
 import org.ros.tf2_ros.StaticTransformBroadcaster;
 import org.ros.tf2_ros.TransformBroadcaster;
 
-import geometry_msgs.Quaternion;
 import geometry_msgs.TransformStamped;
-
-import com.motorola.atap.androidvioservice.VinsServiceHelper;
 
 public class TangoVio extends RosActivity
 {
+    // Set up Transform Broadcasters
     private final TransformBroadcaster mTB = new TransformBroadcaster();
     private final StaticTransformBroadcaster mSTB = new StaticTransformBroadcaster();
+
+    // Set up messages
     private TransformStamped tfs = mTB.newMessage();
+
+    // Set up VIO format
     int coordinateConvention = VinsServiceHelper.STATEFORMAT_UNITY;
 
     public TangoVio() {
