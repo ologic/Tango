f0a696f25de6c342c10f8cc908f86b5c87585e0d
Rohan Agrawal <send2arohan@gmail.com>
2014-07-14 09:55:01 -0700

    Expirementing with getCurrentSuperframe method in TangoPub

--- ROSTango/src/rostango/tango_pub/src/main/java/com/ologicinc/rostango/tango_pub/TangoPub.java
+++ ROSTango/src/rostango/tango_pub/src/main/java/com/ologicinc/rostango/tango_pub/TangoPub.java
@@ -1,36 +1,84 @@
+/*
+ * Copyright 2013 Motorola Mobility LLC. Part of ProjectTango.
+ * CONFIDENTIAL. AUTHORIZED USE ONLY. DO NOT REDISTRIBUTE.
+ *
+ */
+
 package com.ologicinc.rostango.tango_pub;
 
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
-import com.ologicinc.rostango.tango_depth.RosDepthViewSurface;
+import com.motorola.atap.androidvioservice.VinsServiceHelper;
+import com.ologicinc.rostango.TangoNodes.depth.RosDepthViewSurface;
 
-import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
-import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
 public class TangoPub extends RosActivity {
-    private RosDepthViewSurface mDepthView;
+    RosDepthViewSurface mDepthView = null;  //Surface for drawing on.
+    private VinsServiceHelper  mVinsServiceHelper;
 
     public TangoPub() {
         // The RosActivity constructor configures the notification title and ticker
         // messages.
-        super("TangoRosPublisher", "TangoRosPublisher");
+        super("Tango Pub", "Tango Pub");
     }
 
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layoutdepth);
-        mDepthView = (RosDepthViewSurface) findViewById(R.id.depthview);
+
+        mVinsServiceHelper = new VinsServiceHelper(this);
+
+        // Set window to full screen view with no title.
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main);
+//        mDepthView = (RosDepthViewSurface) findViewById(R.id.depthview);
     }
 
     @Override
     protected void init(NodeMainExecutor nodeMainExecutor) {
-        NodeConfiguration nodeConfiguration =
-                NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(), getMasterUri());
-        nodeConfiguration.setMasterUri(getMasterUri());
+//        VioNode vioNode = new VioNode(mVinsServiceHelper);
+//        NodeConfiguration nodeConfiguration =
+//                NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(), getMasterUri());
+//        nodeConfiguration.setMasterUri(getMasterUri());
+
+        ParcelFileDescriptor spp = mVinsServiceHelper.getCurrentSuperFrame();
+        FileDescriptor spfd = spp.getFileDescriptor();
+        FileInputStream spfis  = new FileInputStream(spfd);
+        byte buffer[] = null;
+        try {
+            Log.i("Superframe", Integer.toString(spfis.read(buffer)));
+            Log.i("Superframe", buffer.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Superframe", spp.toString());
+        Log.i("Superframe", spfd.toString());
+
+//        nodeMainExecutor.execute(mDepthView, nodeConfiguration);
+//        nodeMainExecutor.execute(vioNode, nodeConfiguration);
     }
+
+//    @Override
+//    protected void onResume() {
+//        mDepthView.startCamera();
+//        super.onResume();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        mDepthView.stopCamera();
+//        super.onPause();
+//    }
 }
--- ROSTango/src/rostango/tango_vio/src/main/java/com/ologicinc/rostango/tango_vio/TangoTfPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.ologicinc.rostango.tango_vio;
-
-/**
- * Created by rohan on 7/8/14.
- */
-public class TangoTfPublisher{
-
-}
