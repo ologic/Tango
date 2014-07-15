5293560c2f4d4bef9ed152ac88fa0abd375ff383
Rohan Agrawal <send2arohan@gmail.com>
2014-07-15 09:08:24 -0700

    Removed rosjava android tutorials.

--- ROSTango/src/rostango/android_tutorial_pubsub/src/main/gen/org/ros/android/android_tutorial_pubsub/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.ros.android.android_tutorial_pubsub;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
