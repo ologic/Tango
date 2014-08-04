d4d7af685057f664df1d543a159b10dd53423363
Brandon Blodget <brandon@ologicinc.com>
2014-08-04 11:32:12 -0700

    Integrated serial launcher activity into tango_serial

--- ROSTango/src/rostango/tango_serial/src/main/java/com/ologicinc/rostango/tango_serial/TangoSerial.java
+++ ROSTango/src/rostango/tango_serial/src/main/java/com/ologicinc/rostango/tango_serial/TangoSerial.java
@@ -17,7 +17,12 @@
 package com.ologicinc.rostango.tango_serial;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+// import android.util.Log;
+// import android.widget.Toast;
 
+import com.github.ologic.android_ologic.usbserial.driver.UsbSerialDriver;
 import com.motorola.atap.androidvioservice.VinsServiceHelper;
 import com.ologicinc.rostango.TangoNodes.vio.VioNode;
 
@@ -28,6 +33,13 @@ import org.ros.node.NodeMainExecutor;
 
 public class TangoSerial extends RosActivity {
    private VinsServiceHelper  mVinsServiceHelper;
+
+   // USB serial communication.
+   // Driver instance, passed in statically via show(Context, UsbSerialDriver).
+   // It'd be cleaner to re-create the driver using
+   // arguments passed in with the startActivity(Intent) intent.
+   // But this works for now.
+   private static UsbSerialDriver mSerialDriver = null;
 
    public TangoSerial() {
         super("TangoSerial", "TangoSerial");
@@ -50,6 +62,17 @@ public class TangoSerial extends RosActivity {
                 NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress(), getMasterUri());
         nodeConfiguration.setMasterUri(getMasterUri());
         nodeMainExecutor.execute(vioNode, nodeConfiguration);
+    }
+
+    // Starts the activity, using the supplied node executor and driver instance.
+    static public void show(Context context, UsbSerialDriver driver) {
+        // Set our static variables
+        mSerialDriver = driver;
+
+        // Show the activity
+        final Intent intent = new Intent(context, TangoSerial.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(intent);
     }
 
 }
