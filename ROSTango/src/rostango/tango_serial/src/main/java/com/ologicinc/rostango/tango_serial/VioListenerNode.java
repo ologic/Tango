748d155d6cef6de3cd0fdc8da4393d169c4acfdc
Brandon <brandon@brandon-MacBookPro.(none)>
2014-08-28 04:05:16 -0700

    Added Roll, pitch, and yaw data to stream over serial as well as print to screen.

--- ROSTango/src/rostango/tango_serial/src/main/java/com/ologicinc/rostango/tango_serial/VioListenerNode.java
+++ ROSTango/src/rostango/tango_serial/src/main/java/com/ologicinc/rostango/tango_serial/VioListenerNode.java
@@ -23,6 +23,15 @@ import com.MAVLink.Messages.*;
 import com.MAVLink.Messages.ardupilotmega.*;
 import com.MAVLink.Messages.enums.*;
 
+class MavLinkEuler {
+    public float roll;
+    public float pitch;
+    public float yaw;
+}
+
+class MavLinkDCM {
+    public float m[][] = new float[3][3];
+}
 
 /**
  * Created by brandonb on 8/4/14.
@@ -66,6 +75,83 @@ public class VioListenerNode implements NodeMain {
 
         mSubscriber.addMessageListener(new MessageListener<PoseStamped>() {
             int count = 0;
+            /**
+             * Converts a quaternion to a rotation matrix
+             *
+             * @param quaternion a [w, x, y, z] ordered quaternion (null-rotation being 1 0 0 0)
+             * @param dcm a 3x3 rotation matrix
+             */
+            public void mavlink_quaternion_to_dcm(float quaternion[], MavLinkDCM dcm)
+            {
+                double a = quaternion[0];
+                double b = quaternion[1];
+                double c = quaternion[2];
+                double d = quaternion[3];
+                double aSq = a * a;
+                double bSq = b * b;
+                double cSq = c * c;
+                double dSq = d * d;
+                dcm.m[0][0] =(float) (aSq + bSq - cSq - dSq);
+                dcm.m[0][1] =(float) (2.0 * (b * c - a * d));
+                dcm.m[0][2] =(float) (2.0 * (a * c + b * d));
+                dcm.m[1][0] =(float) (2.0 * (b * c + a * d));
+                dcm.m[1][1] =(float) (aSq - bSq + cSq - dSq);
+                dcm.m[1][2] =(float) (2.0 * (c * d - a * b));
+                dcm.m[2][0] =(float) (2.0 * (b * d - a * c));
+                dcm.m[2][1] =(float) (2.0 * (a * b + c * d));
+                dcm.m[2][2] =(float) (aSq - bSq - cSq + dSq);
+            }
+
+
+            /**
+             * Converts a rotation matrix to euler angles
+             *
+             * @param dcm a 3x3 rotation matrix
+             * @param roll the roll angle in radians
+             * @param pitch the pitch angle in radians
+             * @param yaw the yaw angle in radians
+             */
+            public void mavlink_dcm_to_euler(MavLinkDCM dcm, MavLinkEuler attitude)
+            {
+                float phi, theta, psi;
+                theta = (float)Math.asin(-dcm.m[2][0]);
+
+                if (Math.abs(theta - (float)Math.PI/2) < 1.0e-3f) {
+                    phi = 0.0f;
+                    psi = ((float)Math.atan2(dcm.m[1][2] - dcm.m[0][1],
+                            dcm.m[0][2] + dcm.m[1][1]) + phi);
+
+                } else if (Math.abs(theta + (float)Math.PI/2) < 1.0e-3f) {
+                    phi = 0.0f;
+                    psi = (float)Math.atan2(dcm.m[1][2] - dcm.m[0][1],
+                            dcm.m[0][2] + dcm.m[1][1] - phi);
+
+                } else {
+                    phi = (float)Math.atan2(dcm.m[2][1], dcm.m[2][2]);
+                    psi = (float)Math.atan2(dcm.m[1][0], dcm.m[0][0]);
+                }
+
+                attitude.roll = phi;
+                attitude.pitch = theta;
+                attitude.yaw = psi;
+            }
+
+            /**
+             * Converts a quaternion to euler angles
+             *
+             * @param quaternion a [w, x, y, z] ordered quaternion (null-rotation being 1 0 0 0)
+             * @param roll the roll angle in radians
+             * @param pitch the pitch angle in radians
+             * @param yaw the yaw angle in radians
+             */
+            void mavlink_quaternion_to_euler(float quaternion[], MavLinkEuler attitude)
+            {
+                MavLinkDCM dcm = new MavLinkDCM();
+                mavlink_quaternion_to_dcm(quaternion, dcm);
+                mavlink_dcm_to_euler(dcm, attitude);
+            }
+
+
             @Override
             public void onNewMessage(PoseStamped message) {
                 if (DEBUG) Log.d(TAG, message.toString());
@@ -80,9 +166,18 @@ public class VioListenerNode implements NodeMain {
                 mavLinkVision.y = (float)message.getPose().getPosition().getY();
                 mavLinkVision.z = ((float)message.getPose().getPosition().getZ()) * (-1);   //Changed orientation to match with NED
 
-                mavLinkVision.pitch = 0.0f;
-                mavLinkVision.roll = 0.0f;
-                mavLinkVision.yaw = 0.0f;
+                float[] q = new float[4];
+                q[0] = (float)message.getPose().getOrientation().getW();
+                q[1] = (float)message.getPose().getOrientation().getX();
+                q[2] = (float)message.getPose().getOrientation().getY();
+                q[3] = (float)message.getPose().getOrientation().getZ();
+
+                MavLinkEuler e = new MavLinkEuler();
+                mavlink_quaternion_to_euler(q, e);
+
+                mavLinkVision.pitch = e.pitch *(-1);
+                mavLinkVision.roll = e.roll;
+                mavLinkVision.yaw = e.yaw *(-1);
 
                 long lt = System.currentTimeMillis() * 1000;
                 mavLinkVision.usec = lt;
@@ -97,12 +192,11 @@ public class VioListenerNode implements NodeMain {
                             mavLinkVision.y,
                             mavLinkVision.z);
 
-                    quat = String.format("quat: x: %.4f, y: %.4f, z: %.4f, w: %.4f\n",
-                            message.getPose().getOrientation().getX(),
-                            message.getPose().getOrientation().getY(),
-                            message.getPose().getOrientation().getZ(),
-                            message.getPose().getOrientation().getW()
-                        );
+                    quat = String.format("\nEuler: Pitch: %.4f, Roll: %.4f, Yaw: %.4f\n",
+                            mavLinkVision.pitch,
+                            mavLinkVision.roll,
+                            mavLinkVision.yaw);
+
                     final String statsDump = pos + "\n\n" + quat;
                     mStatsView.post(new Runnable(){
 
